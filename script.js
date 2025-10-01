e2f5bdbc01fb43943cf5b252682c5898b313ff32
MiyalalMulla <mullamiyalal@gmail.com>
2025-10-01 21:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Show confirmation dialog
+var result = confirm("Do you want to proceed?");
+
+// Check user response
+if (result) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
